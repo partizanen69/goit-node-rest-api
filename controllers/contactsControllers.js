4addb6301c977b65d3f1d8b04fd433518d2ff8b8
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2024-05-04 18:01:56 +0300

    controllers added

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,57 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsServices from '../services/contactsServices.js';
+import { toController } from '../utils/api.js';
 
-export const getAllContacts = (req, res) => {};
+const getAllContacts = async (_, res) => {
+  const contacts = await contactsServices.listContacts();
+  res.status(200).json(contacts);
+};
 
-export const getOneContact = (req, res) => {};
+const getOneContact = async (req, res) => {
+  const contact = await contactsServices.getContactById(req.params.id);
 
-export const deleteContact = (req, res) => {};
+  if (contact) {
+    res.status(200).json(contact);
+    return;
+  }
 
-export const createContact = (req, res) => {};
+  res.status(404).json({ message: 'Not found' });
+};
 
-export const updateContact = (req, res) => {};
+const deleteContact = async (req, res) => {
+  const removedContact = await contactsServices.removeContact(req.params.id);
+
+  if (removedContact) {
+    res.status(200).json(removedContact);
+    return;
+  }
+
+  res.status(404).json({ message: 'Not found' });
+};
+
+const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+  const addedContact = await contactsServices.addContact(name, email, phone);
+  res.status(201).json(addedContact);
+};
+
+const updateContact = async (req, res) => {
+  const updatedContact = await contactsServices.updateContact(
+    req.params.id,
+    req.body
+  );
+
+  if (!updatedContact) {
+    res.status(404).json({ message: 'Not found' });
+    return;
+  }
+
+  res.status(200).json(updatedContact);
+};
+
+export default {
+  getAllContacts: toController(getAllContacts),
+  getOneContact: toController(getOneContact),
+  deleteContact: toController(deleteContact),
+  createContact: toController(createContact),
+  updateContact: toController(updateContact),
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,17 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().required().min(1),
+  email: Joi.string().required().email(),
+  phone: Joi.string().required(),
+});
 
 export const updateContactSchema = Joi.object({
-
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
 })
+  .min(1)
+  .messages({
+    'object.min': 'Body must have at least one field',
+  });
